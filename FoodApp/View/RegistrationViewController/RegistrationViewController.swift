4fd199e26f92508cff9435381992cece9c29f06e
SatenovaAknur2001 <47109731+SatenovaAknur2001@users.noreply.github.com>
2021-02-23 19:45:39 +0600

    Delete any files

--- FoodApp/View/RegistrationViewController/RegistrationViewController.swift
+++ FoodApp/View/RegistrationViewController/RegistrationViewController.swift
@@ -1,18 +1,18 @@
 //
-//  SignUpPage.swift
+//  RegistrationViewController.swift
 //  FoodApp
 //
 //  Created by Акнур on 2/23/21.
 //  Copyright © 2021 Aknur. All rights reserved.
 //
 
-
 import UIKit
-class SignUpViewController: UIViewController {
-    
+
+class RegistrationViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
+        view.backgroundColor = .yellowColor()
     }
+
 }
